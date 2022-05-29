7f2e0630f4db5a3e2ea06b6f94c6e6c6bea541cd
foxercrack <100960130+foxercrack@users.noreply.github.com>
2022-05-29 16:32:33 +0300

    Mask update

--- config.lua
+++ config.lua
@@ -2,9 +2,13 @@ Config = {}
 
 Config.Distance = 3.0
 
+Config.MaskCheck = true
+Config.Mask3d = true
+
 Config.Locale = {
     ["doesntExist"] = 'Player doesnt exist',
     ["aliasRemoved"] = 'Alias was removed',
     ["alreadyHaveAlias"] = 'That player already have alias',
     ["successfullyAlias"] = 'You successfully set alias',
+    ["mask"] = 'Mask',
 }
