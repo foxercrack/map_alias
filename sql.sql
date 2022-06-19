f2a29f57a17fd5b1df48501634d476f36f809cd4
foxercrack <100960130+foxercrack@users.noreply.github.com>
2022-06-19 20:54:54 +0300

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -3,14 +3,12 @@
 --
 
 CREATE TABLE `alias` (
+  `id` int(11) NOT NULL,
   `identifier` varchar(50) NOT NULL,
   `text` varchar(200) NOT NULL,
   `target` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for table `alias`
---
 ALTER TABLE `alias`
-  ADD PRIMARY KEY (`identifier`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
