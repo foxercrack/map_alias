586fbf720b462a61a1924fb2e918d9d9017732ad
foxercrack <100960130+foxercrack@users.noreply.github.com>
2022-06-19 21:27:15 +0300

    Fixed SQL file

--- sql.sql
+++ sql.sql
@@ -9,6 +9,15 @@ CREATE TABLE `alias` (
   `target` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for table `alias`
+--
+ALTER TABLE `alias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for table `alias`
+--
 ALTER TABLE `alias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
